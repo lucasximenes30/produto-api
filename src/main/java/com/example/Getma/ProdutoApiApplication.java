f9d901f834c16819113761b1ab4f10cb1e41daff
lucasximenes30 <lucasximenes177@gmail.com>
2025-05-26 12:00:31 -0300

    adicionando as alterações finais

--- src/main/java/com/example/Getma/ProdutoApiApplication.java
+++ src/main/java/com/example/Getma/ProdutoApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetmaApplication {
+public class ProdutoApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GetmaApplication.class, args);
+		SpringApplication.run(ProdutoApiApplication.class, args);
 	}
 
 }
